4aaf9f9bd6d5295d545863ccf659660ac460df76
r8vnhill <reachme@ravenhill.cl>
2024-10-24 19:48:58 -0300

    ✨ Add file creation to `ModuleSetupTask`
    - 🛠️ Extended `ModuleSetupTask` to support file creation within specified packages.
    - 🔄 Adjusted `setupBoundsModule` to leverage this new functionality, enhancing the setup process.

--- convention-plugins/src/main/kotlin/bounds.gradle.kts
+++ convention-plugins/src/main/kotlin/bounds.gradle.kts
@@ -3,4 +3,21 @@ import tasks.ModuleSetupTask
 tasks.register<ModuleSetupTask>("setupBoundsModule") {
     description = "Creates the base module and files for the bounds lesson"
     module.set("bounds")
+
+    doLast {
+        createFiles(
+            "repo",
+            main to "Repository.kt",
+            main to "Entity.kt",
+            main to "Serializable.kt",
+            test to "RepositoryTest.kt"
+        )
+        createFiles(
+            "notifications",
+            main to "EmailNotificationSystem.kt",
+            main to "NotificationHandler.kt",
+            main to "Notification.kt",
+            test to "NotificationSystemTest.kt"
+        )
+    }
 }
--- convention-plugins/src/main/kotlin/tasks/ModuleSetupTask.kt
+++ convention-plugins/src/main/kotlin/tasks/ModuleSetupTask.kt
@@ -3,6 +3,7 @@ package tasks
 import org.gradle.api.DefaultTask
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.TaskAction
 import java.io.File
 
@@ -12,6 +13,14 @@ abstract class ModuleSetupTask : DefaultTask() {
 
     @get:Input
     abstract val module: Property<String>
+
+    @get:Internal
+    val main: File
+        get() = baseDir.resolve("src/main/kotlin")
+
+    @get:Internal
+    val test: File
+        get() = baseDir.resolve("src/test/kotlin")
 
     private val baseDir: File
         get() = project.rootProject.file(module)
@@ -49,5 +58,20 @@ abstract class ModuleSetupTask : DefaultTask() {
     private fun createBuildFile() = baseDir.resolve("build.gradle.kts").run {
         if (exists()) printError("The build file already exists")
         else writeText("// Intentionally left blank\n")
+    }
+
+    fun createFiles(packageName: String, vararg files: Pair<File, String>) {
+        files.forEach { (dir, name) ->
+            val group = project.rootProject.group.toString()
+            val packageDir = dir.resolve("$group/$packageName".replace(".", "/"))
+            val file = packageDir.resolve(name)
+            if (file.exists()) {
+                printError("The file $name already exists")
+            } else {
+                packageDir.mkdirs()
+                file.writeText("package $group.$packageName\n\n")
+                println("The file $name was created successfully")
+            }
+        }
     }
 }
